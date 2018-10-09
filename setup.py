72a9ebd2a846d8798fbad2d14bb809b39d9d1e4e
Marius Feldmann <feldmann.marius@gmail.com>
2018-10-09 14:01:34 +0200

    updated psycopg dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     py_modules=['pg_table_markdown'],
     install_requires=[
         'Click==6.2',
-        'psycopg2==2.6.1',
+        'psycopg2==2.7.4',
     ],
     entry_points='''
         [console_scripts]
